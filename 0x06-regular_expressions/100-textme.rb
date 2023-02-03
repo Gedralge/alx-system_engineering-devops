bbc07989745c1bef4681c2e14420876b16b3b050
Gedralge <Gedralgerror@mail.com>
2023-02-03 11:00:17 -0800

    100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,4 @@
-#!/usr/bin/env rubby
-puts ARGV[0].scan(/(?<=\from:|to:|flags:)[^\]]*/).join(',')
+#!/usr/bin/env ruby
+#print ARGV[0].scan(/\[from:([a-zA-Z]+|[+0-9]{11,12})\]/).join,","
+#print ARGV[0].scan(/\[to:([a-zA-Z]+|[+0-9]{11,12})\]/).join,","
+#puts  ARGV[0].scan(/\[flags:([-01:]+)\]/).join
